d5d916fb4b9fc231e8c5f74bf6943fd31b1159aa
Leon Du <leondu@gmail.com>
2011-11-24 22:18:38 +0800

    add ALIAS mapping

--- lib/active_merchant_ogone.rb
+++ lib/active_merchant_ogone.rb
@@ -8,23 +8,22 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       module Ogone
-        
-        INBOUND_ENCRYPTED_VARIABLES = %w(
-  AAVADDRESS AAVCHECK AAVZIP ACCEPTANCE ALIAS AMOUNT BRAND CARDNO CCCTY CN COMPLUS CREATION_STATUS CURRENCY CVCCHECK DCC_COMMPERCENTAGE DCC_CONVAMOUNT DCC_CONVCCY DCC_EXCHRATE DCC_EXCHRATESOURCE DCC_EXCHRATETS DCC_INDICATOR DCC_MARGINPERCENTAGE DCC_VALIDHOURS DIGESTCARDNO ECI ED ENCCARDNO IP IPCTY NBREMAILUSAGE NBRIPUSAGE NBRIPUSAGE_ALLTX NBRUSAGE NCERROR ORDERID PAYID PM SCO_CATEGORY SCORING SHA-OUT STATUS SUBSCRIPTION_ID TRXDATE VC )
-        
+
+        INBOUND_ENCRYPTED_VARIABLES = %w(AAVADDRESS AAVCHECK AAVZIP ACCEPTANCE ALIAS AMOUNT BRAND CARDNO CCCTY SHA-OUT CN COMPLUS CURRENCY CVCCHECK DCC_COMMPERCENTAGE DCC_CONVAMOUNT DCC_CONVCCY DCC_EXCHRATE DCC_EXCHRATESOURCE DCC_EXCHRATETS DCC_INDICATOR DCC_MARGINPERCENTAGE DCC_VALIDHOUS DIGESTCARDNO ECI ED ENCCARDNO IP IPCTY NBREMAILUSAGE NBRIPUSAGE NBRIPUSAGE_ALLTX NBRUSAGE NCERROR ORDERID PAYID PM SCO_CATEGORY SCORING STATUS TRXDATE VC)
+
         mattr_accessor :inbound_signature
         mattr_accessor :outbound_signature
-        
+
         mattr_accessor :test_service_url
         mattr_accessor :live_service_url
-        
+
         self.test_service_url = 'https://secure.ogone.com/ncol/test/orderstandard.asp'
         self.live_service_url = 'https://secure.ogone.com/ncol/prod/orderstandard.asp'
-        
+
         def self.setup
           yield(self)
         end
-        
+
         def self.service_url
           mode = ActiveMerchant::Billing::Base.integration_mode
           case mode
@@ -34,26 +33,26 @@ module ActiveMerchant #:nodoc:
             raise StandardError, "Integration mode set to an invalid value: #{mode}"
           end
         end
-        
+
         def self.notification(post, options={})
           Notification.new(post, options={})
         end
-        
+
         def self.outbound_message_signature(fields, signature=nil)
           signature ||= self.outbound_signature
           datastring = fields.select {|k, v| !v.blank? }.
                               sort_by {|k, v| k.upcase }.
                               collect{|key, value| "#{key.upcase}=#{value}#{signature}"}.join
-                              
+
           Digest::SHA1.hexdigest(datastring).upcase
         end
-        
+
         def self.inbound_message_signature(fields, signature=nil)
           signature ||= self.inbound_signature
           datastring = fields.select  {|k, v| !v.blank? && INBOUND_ENCRYPTED_VARIABLES.include?(k.upcase) }.
-                              sort_by {|k, v| INBOUND_ENCRYPTED_VARIABLES.index(k.upcase) }.
+                              sort_by {|k, v| k.upcase }.
                               collect {|key, value| "#{key.upcase}=#{value}#{signature}"}.join
-                             
+
           Digest::SHA1.hexdigest(datastring).upcase
         end
       end
--- lib/active_merchant_ogone/helper.rb
+++ lib/active_merchant_ogone/helper.rb
@@ -3,35 +3,78 @@ module ActiveMerchant #:nodoc:
     module Integrations #:nodoc:
       module Ogone
         class Helper < ActiveMerchant::Billing::Integrations::Helper
-          
+
+          # see full parameter list in your Ogone Backend under 'Support' -> 'Integration & User manuals'
+
           # required
-          mapping :order,    'orderID'
+          mapping :order,    'ORDERID'
           mapping :account,  'PSPID'
-          mapping :amount,   'amount'
-          mapping :currency, 'currency'
+          mapping :amount,   'AMOUNT'
+          mapping :currency, 'CURRENCY'
+          mapping :language, 'LANGUAGE'
 
-          # optional - TODO
-          mapping :billing_address, :city     => 'ownertown',
-                                    :address1 => 'owneraddress',
-                                    :zip      => 'ownerZIP',
-                                    :country  => 'ownercty'
+          mapping :payment, 'PM'
+          mapping :payment_list, 'PMLIST'
+          mapping :payment_list_type, 'PMLISTTYPE'
 
-          # mapping :description, 'COM'
-          # mapping :tax, ''
-          # mapping :shipping, ''
+          mapping :device, 'DEVICE' # e.g. 'mobile'; only works with paypal
 
+          mapping :customer, :first_name => 'ECOM_BILLTO_POSTAL_NAME_FIRST',
+                             :last_name  => 'ECOM_BILLTO_POSTAL_NAME_LAST',
+                             :email      => 'EMAIL'
+
+          mapping :billing_address, :city     => 'OWNERTOWN',
+                                    :address1 => 'OWNERADDRESS',
+                                    :address2 => 'OWNERADDRESS2',
+                                    :zip      => 'OWNERZIP',
+                                    :country  => 'OWNERCTY'
+
+          mapping :shipping_address,  :first_name => 'ECOM_SHIPTO_POSTAL_NAME_FIRST',
+                                      :last_name => 'ECOM_SHIPTO_POSTAL_NAME_LAST',
+                                      :company => 'ECOM_SHIPTO_COMPANY',
+                                      :phone_number => 'ECOM_SHIPTO_TELECOM_PHONE_NUMBER',
+                                      :email => 'ECOM_SHIPTO_ONLINE_EMAIL',
+                                      :city     => 'ECOM_SHIPTO_POSTAL_CITY',
+                                      :address1 => 'ECOM_SHIPTO_POSTAL_STREET_LINE1',
+                                      :address2 => 'ECOM_SHIPTO_POSTAL_STREET_LINE2',
+                                      :zip      => 'ECOM_SHIPTO_POSTAL_POSTALCODE',
+                                      :country  => 'ECOM_SHIPTO_POSTAL_COUNTRYCODE'
+
+          mapping :layout,  :title => 'TITLE',
+                            :bg_color => 'BGCOLOR',
+                            :txt_color => 'TXTCOLOR',
+                            :table_bg_color => 'TBLBGCOLOR',
+                            :table_txt_color => 'TBLTXTCOLOR',
+                            :button_bg_color => 'BUTTONBGCOLOR',
+                            :button_txt_color => 'BUTTONTXTCOLOR',
+                            :font_type => 'FONTTYPE',
+                            :logo_name => 'LOGO',
+                            :template_path => 'TP'
+
+          mapping :description, 'COM'
+          mapping :hidden_description, 'COMPLUS'
+          mapping :additional_params, 'PARAMPLUS'
+          
           # redirection
-          mapping :redirect, :accepturl    => 'accepturl',
-                             :declineurl   => 'declineurl',
-                             :cancelurl    => 'cancelurl',
-                             :exceptionurl => 'exceptionurl'
-                             
+          mapping :redirect, :accepturl    => 'ACCEPTURL',
+                             :declineurl   => 'DECLINEURL',
+                             :cancelurl    => 'CANCELURL',
+                             :exceptionurl => 'EXCEPTIONURL',
+                             :backurl => 'BACKURL',
+                             :homeurl => 'HOMEURL',
+                             :catalogurl => 'CATALOGURL'
+
+
+           # ALIAS management
+           mapping :payment_alias, 'ALIAS'
+           mapping :payment_alias_usage, 'ALIASUSAGE'
+
           def customer(mapping = {})
-            add_field('ownertelno', mapping[:phone])
+            add_field('OWNERTELNO', mapping[:phone])
             add_field('EMAIL', mapping[:email])
             add_field('CN', "#{mapping[:first_name]} #{mapping[:last_name]}")
           end
-          
+
           def operation operation
             op = case operation
             when :authorization, :auth; 'RES'
@@ -47,13 +90,13 @@ module ActiveMerchant #:nodoc:
             add_field('SHASign', outbound_message_signature)
             super
           end
-          
+
         private
-          
+
           def outbound_message_signature
             Ogone.outbound_message_signature(@fields)
           end
-          
+
         end
       end
     end
